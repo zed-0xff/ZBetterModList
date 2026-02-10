84360e740d7df773b09394680e1e09e2b353402c
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-02-10 17:02:05 +0200

    upd

--- 42/media/java/client/src/Main.java
+++
@@ -1,9 +0,0 @@
-package me.zed_0xff.zb_better_modlist;
-
-import me.zed_0xff.zombie_buddy.Exposer;
-
-public class Main {
-    public static void main(String[] args) {
-        Exposer.exposeClassToLua(ModListHelper.class);
-    }
-}
